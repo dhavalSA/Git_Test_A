10204d3d04c7dfb9d864f0a13309289066c343cd
Dhaval Soni <62322557+dhavalSA@users.noreply.github.com>
2023-02-27 18:17:52 +0530

    First edit

--- Git_Test_A/ViewController.swift
+++ Git_Test_A/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("This is 1")
+        print("This is 2")
+        print("this is 3")
     }
 
 
